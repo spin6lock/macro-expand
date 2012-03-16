48673f14fe8e40e5aa45c025c655e9cdd38b106f
spin6lock <john.37@gmail.com>
2012-03-16 14:50:42 +0800

    First blood

---
+++ mymacro.py
@@ -0,0 +1,36 @@
+#-*- coding:utf8 -*-
+import argparse
+
+mark = "#macro "
+def unfold_macro(filename):
+	with open(filename, 'r') as fh_in, \
+		open(filename + '.out', 'w') as fh_out:
+		lines = fh_in.readlines()
+		for line in lines:
+			pos = line.find(mark) 
+			if pos == -1:
+				fh_out.write(line)
+				continue
+			macro_filename = extract_macro_filename(pos, line)
+			write_macro(fh_out, macro_filename)	
+
+def extract_macro_filename(pos, line):
+	start = pos + len(mark)
+	end_pos = line[start].find(" ")
+	if end_pos == -1:
+		filename = line[start:]
+	else:
+		filename = line[start:end_pos]
+	return filename.strip()
+
+def write_macro(fh_out, macro_filename):
+	with open(macro_filename, 'r') as fh_in:
+		lines = fh_in.readlines()
+		fh_out.writelines(lines)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='This is a \
+		simple program to expand macro in python source code')
+	parser.add_argument('sourcefile', action='store')
+	namespace = parser.parse_args()
+	unfold_macro(namespace.source)
